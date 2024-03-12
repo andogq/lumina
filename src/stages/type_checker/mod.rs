83ce7ac3403da4734a5c7043ade9bf65a30ddf5a
Tom Anderson <tom@ando.gq>
2024-03-12 22:22:25 +1100

    More type checking for expressions

--- src/stages/type_checker/mod.rs
+++ src/stages/type_checker/mod.rs
@@ -1,6 +1,9 @@
 use std::collections::HashMap;
 
-use crate::core::ast::{Expression, InfixExpression, PrefixExpression};
+use crate::core::ast::{
+    Block, Expression, InfixExpression, LetStatement, PrefixExpression, Program, ReturnStatement,
+    Statement,
+};
 
 /// Primitive types available in the language.
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -8,12 +11,66 @@ pub enum Type {
     Boolean,
     Integer,
     String,
+    Unit,
 }
 
 /// A map between variable names and their associated type
 type Context = HashMap<String, Type>;
 
-fn type_check_expression(e: &Expression, ctx: &Context) -> Result<Type, &'static str> {
+pub fn type_check_program(p: Program) -> Result<Context, &'static str> {
+    // Set up a new type context
+    let mut ctx = Context::new();
+
+    for statement in p.statements {
+        let ty = type_check_statement(&statement, &mut ctx)?;
+
+        if ty != Type::Unit {
+            return Err("cannot return a value from a program");
+        }
+    }
+
+    Ok(ctx)
+}
+
+pub fn type_check_block(b: &Block, ctx: &mut Context) -> Result<Type, &'static str> {
+    let mut ty = Type::Unit;
+
+    for statement in &b.statements {
+        ty = type_check_statement(&statement, ctx)?;
+    }
+
+    Ok(ty)
+}
+
+fn type_check_statement(s: &Statement, ctx: &mut Context) -> Result<Type, &'static str> {
+    match s {
+        Statement::Let(LetStatement { name, value, .. }) => {
+            // Determine the type of the value
+            let ty = type_check_expression(value, ctx)?;
+
+            // Update the context so the variable has the correct type
+            ctx.insert(name.value.clone(), ty);
+
+            Ok(Type::Unit)
+        }
+        Statement::Return(ReturnStatement { value, .. }) => Ok(type_check_expression(value, ctx)?),
+        Statement::Expression {
+            expression,
+            semicolon,
+        } => {
+            let ty = type_check_expression(expression, ctx)?;
+
+            Ok(if *semicolon {
+                // Implicit return
+                ty
+            } else {
+                Type::Unit
+            })
+        }
+    }
+}
+
+fn type_check_expression(e: &Expression, ctx: &mut Context) -> Result<Type, &'static str> {
     match e {
         Expression::Identifier(ident) => ctx
             .get(&ident.value)
@@ -38,7 +95,7 @@ fn type_check_expression(e: &Expression, ctx: &Context) -> Result<Type, &'static
                 Err("expected left and right side of infix operation to be identical")
             }
         }
-        Expression::Block(_) => todo!(),
+        Expression::Block(block) => type_check_block(block, ctx),
         Expression::If(_) => todo!(),
         Expression::Function(_) => todo!(),
         Expression::Call(_) => todo!(),
@@ -47,7 +104,7 @@ fn type_check_expression(e: &Expression, ctx: &Context) -> Result<Type, &'static
 
 #[cfg(test)]
 mod test {
-    use crate::core::ast::{Identifier, InfixExpression, InfixOperatorToken};
+    use crate::core::ast::{Identifier, InfixExpression, InfixOperatorToken, IntegerLiteral};
 
     use super::*;
 
@@ -67,11 +124,60 @@ mod test {
             operator_token: InfixOperatorToken::Plus(Default::default()),
         });
 
-        let ctx = Context::from_iter([
+        let mut ctx = Context::from_iter([
             ("x".to_string(), Type::Integer),
             ("y".to_string(), Type::Integer),
         ]);
 
-        assert_eq!(type_check_expression(&e, &ctx).unwrap(), Type::Integer);
+        assert_eq!(type_check_expression(&e, &mut ctx).unwrap(), Type::Integer);
+    }
+
+    #[test]
+    fn simple_program() {
+        let p = Program {
+            statements: vec![
+                Statement::Let(LetStatement {
+                    let_token: Default::default(),
+                    name: Identifier {
+                        value: "a".to_string(),
+                        ident_token: Default::default(),
+                    },
+                    value: Expression::Integer(IntegerLiteral::new(5)),
+                }),
+                Statement::Let(LetStatement {
+                    let_token: Default::default(),
+                    name: Identifier {
+                        value: "b".to_string(),
+                        ident_token: Default::default(),
+                    },
+                    value: Expression::Integer(IntegerLiteral::new(10)),
+                }),
+                Statement::Let(LetStatement {
+                    let_token: Default::default(),
+                    name: Identifier {
+                        value: "c".to_string(),
+                        ident_token: Default::default(),
+                    },
+                    value: Expression::Infix(InfixExpression {
+                        operator_token: InfixOperatorToken::Plus(Default::default()),
+                        operator: "+".to_string(),
+                        left: Box::new(Expression::Identifier(Identifier {
+                            value: "a".to_string(),
+                            ident_token: Default::default(),
+                        })),
+                        right: Box::new(Expression::Identifier(Identifier {
+                            value: "b".to_string(),
+                            ident_token: Default::default(),
+                        })),
+                    }),
+                }),
+            ],
+        };
+
+        let ctx = type_check_program(p).unwrap();
+
+        assert_eq!(ctx.get("a").unwrap(), &Type::Integer);
+        assert_eq!(ctx.get("b").unwrap(), &Type::Integer);
+        assert_eq!(ctx.get("c").unwrap(), &Type::Integer);
     }
 }
