9c11acde5f067a5d559f4f21dec7c32983a78a41
Tom Anderson <tom@ando.gq>
2024-04-14 22:33:27 +1000

    terrible IR to represent returning an integer

---
+++ src/core/ir/index.rs
@@ -0,0 +1,52 @@
+use std::{fmt::Debug, marker::PhantomData};
+
+pub struct Index<T>(pub(super) usize, pub(super) PhantomData<T>);
+impl<T> PartialEq for Index<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.0 == other.0
+    }
+}
+impl<T> Eq for Index<T> {}
+impl<T> Debug for Index<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Index({})", self.0)
+    }
+}
+impl<T> Clone for Index<T> {
+    fn clone(&self) -> Self {
+        Self(self.0, self.1.clone())
+    }
+}
+impl<T> Copy for Index<T> {}
+impl<T> Index<T> {
+    pub fn new(i: usize) -> Self {
+        Self(i, PhantomData::default())
+    }
+}
+
+pub struct IndexVec<T>(Vec<T>, PhantomData<Index<T>>);
+impl<T> Default for IndexVec<T> {
+    fn default() -> Self {
+        Self(Default::default(), Default::default())
+    }
+}
+impl<T> std::ops::Index<Index<T>> for IndexVec<T> {
+    type Output = T;
+
+    fn index(&self, index: Index<T>) -> &Self::Output {
+        self.0.index(index.0)
+    }
+}
+impl<T> IndexVec<T> {
+    pub fn push(&mut self, value: T) -> Index<T> {
+        let i = Index::new(self.0.len());
+
+        self.0.push(value);
+
+        i
+    }
+
+    pub fn get(&self, index: Index<T>) -> Option<&T> {
+        self.0.get(index.0)
+    }
+}
--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,4 +1,5 @@
 pub mod ast;
+pub mod ir;
 pub mod lexer;
 pub mod parse;
 pub mod symbol;
