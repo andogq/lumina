d02e321e4230ec13cd11d15b841d8f7a686dd672
Tom Anderson <tom@ando.gq>
2024-07-01 20:13:16 +1000

    switch to rstest

--- src/core/lexer/mod.rs
+++ src/core/lexer/mod.rs
@@ -111,8 +111,67 @@ impl Lexer {
 #[cfg(test)]
 mod test {
     use super::*;
+    use rstest::rstest;
 
-    fn test_tokens(source: &'static str, expected: &[Token]) {
+    #[rstest]
+    #[case::empty("", &[])]
+    #[case::single_digit_integer("1", &[Token::integer("1")])]
+    #[case::multi_digit_integer("123", &[Token::integer("123")])]
+    #[case::multi_integer("123 456 999", &[Token::integer("123"), Token::integer("456"), Token::integer("999")])]
+    #[case::single_char_ident("a", &[Token::ident("a")])]
+    #[case::multi_char_ident("asdf", &[Token::ident("asdf")])]
+    #[case::multi_ident("asdf some ident", &[Token::ident("asdf"), Token::ident("some"), Token::ident("ident")])]
+    #[case::plus("+", &[Token::plus()])]
+    #[case::equals("=", &[Token::equals()])]
+    #[case::semicolon(";", &[Token::semicolon()])]
+    #[case::thin_arrow("->", &[Token::thin_arrow()])]
+    #[case::left_paren("(", &[Token::left_paren()])]
+    #[case::right_paren(")", &[Token::right_paren()])]
+    #[case::left_brace("{", &[Token::left_brace()])]
+    #[case::right_brace("}", &[Token::right_brace()])]
+    #[case::t_true("true", &[Token::t_true()])]
+    #[case::t_false("false", &[Token::t_false()])]
+    #[case::t_fn("fn", &[Token::t_fn()])]
+    #[case::t_return("return", &[Token::t_return()])]
+    #[case::t_let("let", &[Token::t_let()])]
+    #[case::t_if("if", &[Token::t_if()])]
+    #[case::t_else("else", &[Token::t_else()])]
+    #[case::trueish("trueish", &[Token::ident("trueish")])]
+    #[case::falseish("falseish", &[Token::ident("falseish")])]
+    #[case::fnish("fnish", &[Token::ident("fnish")])]
+    #[case::returnish("returnish", &[Token::ident("returnish")])]
+    #[case::letish("letish", &[Token::ident("letish")])]
+    #[case::ifish("ifish", &[Token::ident("ifish")])]
+    #[case::elseish("elseish", &[Token::ident("elseish")])]
+    #[case::simple_addition("1 + 3;", &[
+        Token::integer("1"),
+        Token::plus(),
+        Token::integer("3"),
+        Token::semicolon(),
+    ])]
+    #[case::all_tokens(
+        "123 ident + = ; -> ( ) { } true false fn return let if else",
+        &[
+            Token::integer("123"),
+            Token::ident("ident"),
+            Token::plus(),
+            Token::equals(),
+            Token::semicolon(),
+            Token::thin_arrow(),
+            Token::left_paren(),
+            Token::right_paren(),
+            Token::left_brace(),
+            Token::right_brace(),
+            Token::t_true(),
+            Token::t_false(),
+            Token::t_fn(),
+            Token::t_return(),
+            Token::t_let(),
+            Token::t_if(),
+            Token::t_else(),
+        ],
+    )]
+    fn test_tokens(#[case] source: &'static str, #[case] expected: &[Token]) {
         let mut lexer = Lexer::new(Source::new(source));
 
         let tokens = std::iter::from_fn(|| match lexer.next_token() {
@@ -122,203 +181,5 @@ mod test {
         .collect::<Vec<_>>();
 
         assert_eq!(tokens, expected);
-    }
-
-    #[test]
-    fn empty() {
-        test_tokens("", &[]);
-    }
-
-    #[test]
-    fn single_digit_integer() {
-        test_tokens("1", &[Token::integer("1")]);
-    }
-
-    #[test]
-    fn multi_digit_integer() {
-        test_tokens("123", &[Token::integer("123")]);
-    }
-
-    #[test]
-    fn multiple_integers() {
-        test_tokens(
-            "123 456 999",
-            &[
-                Token::integer("123"),
-                Token::integer("456"),
-                Token::integer("999"),
-            ],
-        );
-    }
-
-    #[test]
-    fn single_character_ident() {
-        test_tokens("a", &[Token::ident("a")]);
-    }
-
-    #[test]
-    fn multi_character_ident() {
-        test_tokens("asdf", &[Token::ident("asdf")]);
-    }
-
-    #[test]
-    fn mulitple_idents() {
-        test_tokens(
-            "asdf some ident",
-            &[
-                Token::ident("asdf"),
-                Token::ident("some"),
-                Token::ident("ident"),
-            ],
-        );
-    }
-
-    #[test]
-    fn token_plus() {
-        test_tokens("+", &[Token::plus()]);
-    }
-
-    #[test]
-    fn token_equals() {
-        test_tokens("=", &[Token::equals()]);
-    }
-
-    #[test]
-    fn token_semicolon() {
-        test_tokens(";", &[Token::semicolon()]);
-    }
-
-    #[test]
-    fn token_thin_arrow() {
-        test_tokens("->", &[Token::thin_arrow()]);
-    }
-
-    #[test]
-    fn token_left_paren() {
-        test_tokens("(", &[Token::left_paren()]);
-    }
-
-    #[test]
-    fn token_right_paren() {
-        test_tokens(")", &[Token::right_paren()]);
-    }
-
-    #[test]
-    fn token_left_brace() {
-        test_tokens("{", &[Token::left_brace()]);
-    }
-
-    #[test]
-    fn token_right_brace() {
-        test_tokens("}", &[Token::right_brace()]);
-    }
-
-    #[test]
-    fn token_true() {
-        test_tokens("true", &[Token::t_true()]);
-    }
-
-    #[test]
-    fn token_false() {
-        test_tokens("false", &[Token::t_false()]);
-    }
-
-    #[test]
-    fn token_fn() {
-        test_tokens("fn", &[Token::t_fn()]);
-    }
-
-    #[test]
-    fn token_return() {
-        test_tokens("return", &[Token::t_return()]);
-    }
-
-    #[test]
-    fn token_let() {
-        test_tokens("let", &[Token::t_let()]);
-    }
-
-    #[test]
-    fn token_if() {
-        test_tokens("if", &[Token::t_if()]);
-    }
-
-    #[test]
-    fn token_else() {
-        test_tokens("else", &[Token::t_else()]);
-    }
-
-    #[test]
-    fn ident_trueish() {
-        test_tokens("trueish", &[Token::ident("trueish")]);
-    }
-
-    #[test]
-    fn ident_falseish() {
-        test_tokens("falseish", &[Token::ident("falseish")]);
-    }
-
-    #[test]
-    fn ident_fnish() {
-        test_tokens("fnish", &[Token::ident("fnish")]);
-    }
-
-    #[test]
-    fn ident_returnish() {
-        test_tokens("returnish", &[Token::ident("returnish")]);
-    }
-
-    #[test]
-    fn ident_letish() {
-        test_tokens("letish", &[Token::ident("letish")]);
-    }
-
-    #[test]
-    fn ident_ifish() {
-        test_tokens("ifish", &[Token::ident("ifish")]);
-    }
-
-    #[test]
-    fn ident_elseish() {
-        test_tokens("elseish", &[Token::ident("elseish")]);
-    }
-
-    #[test]
-    fn simple_addition() {
-        test_tokens(
-            "1 + 3;",
-            &[
-                Token::integer("1"),
-                Token::plus(),
-                Token::integer("3"),
-                Token::semicolon(),
-            ],
-        );
-    }
-
-    #[test]
-    fn all_tokens() {
-        test_tokens(
-            "123 ident + = ; -> ( ) { } true false fn return let if else",
-            &[
-                Token::integer("123"),
-                Token::ident("ident"),
-                Token::plus(),
-                Token::equals(),
-                Token::semicolon(),
-                Token::thin_arrow(),
-                Token::left_paren(),
-                Token::right_paren(),
-                Token::left_brace(),
-                Token::right_brace(),
-                Token::t_true(),
-                Token::t_false(),
-                Token::t_fn(),
-                Token::t_return(),
-                Token::t_let(),
-                Token::t_if(),
-                Token::t_else(),
-            ],
-        );
     }
 }
